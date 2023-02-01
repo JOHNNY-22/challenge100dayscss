c54a31439afdc9fc4ce6f1a31688747d4e0846e5
Johnny <johnny.altes1@gmail.com>
2023-02-01 20:19:32 +0100

    Dia 1 y 2

---
+++ challenge100dayscss/src/components/day1/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+function day1() {
+	return (
+    <div class="frame">
+  		<div class="center">
+			<div class="number">
+				<div class="one-one"></div>
+				<div class="one-two"></div>
+				<div class="zero-one"></div>
+				<div class="zero-two"></div>
+			</div>
+			<div class="divtext">
+		<span class="big">Days</span>
+		<br/>
+		<span class="small">CSS CHALLENGE</span>
+		</div>
+	</div>
+</div>
+);
+}
+export default day1;
